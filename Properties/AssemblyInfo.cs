8b02dc940f9d0a722b7c991e8159efca80fae9a6
Andrey Shchekin <ashmind@gmail.com>
2014-05-04 01:36:15 +1200

    Release 0.7.0.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -3,9 +3,9 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Argument")]
+[assembly: AssemblyTitle("AshMind.IO.Abstractions")]
 [assembly: AssemblyCompany("Andrey Shchekin")]
-[assembly: AssemblyProduct("Argument")]
+[assembly: AssemblyProduct("AshMind.IO.Abstractions")]
 [assembly: AssemblyCopyright("Copyright © Andrey Shchekin 2013–2014")]
 
 // Version information for an assembly consists of the following four values:
@@ -18,5 +18,5 @@
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
